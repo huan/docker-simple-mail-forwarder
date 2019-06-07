11827ca8b51ee84b5b810806748e691f04ccef8e
Huan LI <zixia@zixia.net>
2019-06-08 00:19:39 +0800

    Fix leak of EC Cert/Key problem (#58)

--- install/init-openssl.sh
+++ install/init-openssl.sh
@@ -7,11 +7,31 @@
 cd /etc/postfix/cert
 # skip generation of certificate if one exists (by mounting a volume)
 if [ ! -f "smtp.cert" ] || [ ! -f "smtp.ec.cert" ]; then
-    #openssl dhparam -2 -out dh_512.pem 512
-    #openssl dhparam -2 -out dh_1024.pem 1024
-    openssl req -new -outform PEM -out smtp.cert -newkey rsa:2048 \
-            -nodes -keyout smtp.key -keyform PEM -days 3650 -x509 \
-            -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}"
+    openssl req \
+        -new \
+        -outform PEM \
+        -nodes \
+        -keyform PEM \
+        -days 3650 \
+        -x509 \
+        -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
+        \
+        -newkey rsa:2048 \
+        -keyout smtp.key \
+        -out smtp.cert
+
+    openssl req \
+        -new \
+        -outform PEM \
+        -nodes \
+        -keyform PEM \
+        -days 3650 \
+        -x509 \
+        -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
+        \
+        -newkey ec:<(openssl ecparam -name secp384r1) \
+        -keyout smtp.ec.key \
+        -out smtp.ec.cert
 
     chown -R root.postfix /etc/postfix/cert/
     chmod -R 750 /etc/postfix/cert/

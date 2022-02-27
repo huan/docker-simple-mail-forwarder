c79675cc6f66de81034a91eb31f0bfeefe06f9de
Nicky West <nickkwest@gmail.com>
2022-02-27 13:15:48 -0800

    Fix for issue where a signed cert and self signed cert can both exist When any cert exists, do not generate self signed cert or postfix will server both/either Comment out smtpd_tls_cecert or smtpd_tls_cert config if matching cert does not exist

--- install/init-openssl.sh
+++ install/init-openssl.sh
@@ -6,36 +6,49 @@
 
 cd /etc/postfix/cert
 
-# skip generation of certificate if one exists (by mounting a volume)
-if [ ! -f "smtp.cert" ]; then
-    openssl req \
-        -new \
-        -outform PEM \
-        -nodes \
-        -keyform PEM \
-        -days 3650 \
-        -x509 \
-        -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
-        \
-        -newkey rsa:2048 \
-        -keyout smtp.key \
-        -out smtp.cert
-fi
+# If either certificate exists, don't generate any certificates
+if [ -f "smtp.cert" ] || [ -f "smtp.ec.cert" ]; then
+  # If RSA cert does not exist, comment out smtpd_tls_cert_file & smtpd_tls_key_file
+  if [ ! -f "smtp.cert" ]; then
+    sed -ine '/\(smtpd_tls_cert_file\|smtpd_tls_key_file\)/s/^/#/' /etc/postfix/main.cf
+  fi
 
-if [ ! -f "smtp.ec.cert" ]; then
-    openssl req \
-        -new \
-        -outform PEM \
-        -nodes \
-        -keyform PEM \
-        -days 3650 \
-        -x509 \
-        -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
-        \
-        -newkey ec:<(openssl ecparam -name secp384r1) \
-        -keyout smtp.ec.key \
-        -out smtp.ec.cert
-fi
+  # If EC cert does not exist, comment out smtpd_tls_eccert_file & smtpd_tls_eckey_file
+  if [ ! -f "smtp.ec.cert" ]; then
+    sed -ine '/\(smtpd_tls_eccert_file\|smtpd_tls_eckey_file\)/s/^/#/' /etc/postfix/main.cf
+  fi
 
+else
+  # skip generation of certificate if one exists (by mounting a volume)
+  if [ ! -f "smtp.cert" ]; then
+      openssl req \
+          -new \
+          -outform PEM \
+          -nodes \
+          -keyform PEM \
+          -days 3650 \
+          -x509 \
+          -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
+          \
+          -newkey rsa:2048 \
+          -keyout smtp.key \
+          -out smtp.cert
+  fi
+
+  if [ ! -f "smtp.ec.cert" ]; then
+      openssl req \
+          -new \
+          -outform PEM \
+          -nodes \
+          -keyform PEM \
+          -days 3650 \
+          -x509 \
+          -subj "/C=US/ST=Matrix/L=L/O=O/CN=${SMF_DOMAIN:-simple-mail-forwarder.com}" \
+          \
+          -newkey ec:<(openssl ecparam -name secp384r1) \
+          -keyout smtp.ec.key \
+          -out smtp.ec.cert
+  fi
+fi
 chown -R root.postfix /etc/postfix/cert/
 chmod -R 750 /etc/postfix/cert/
